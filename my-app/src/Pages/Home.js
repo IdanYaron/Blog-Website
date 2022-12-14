8495967e4cb371d201165971a730332fd200ca21
IdanYaron <idanyaron05@gmail.com>
2022-12-14 20:06:50 +0200

    Add useEffect for array of posts

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../firebase-config'
 
 function Home() {
+  const [postList, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  })
+
   return (
-    <div>Home</div>
+    <div>()
+
+    </div>
   )
 }
 
